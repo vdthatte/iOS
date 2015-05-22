b65761dc76c44db06ee0fced4d47b79076d01ef4
Vidyadhar Thatte <vd_thatte@yahoo.com>
2015-05-21 23:37:40 -0400

    May 21 11:37 PM

---
+++ animations/animations/TransitionDestinationVC.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionDestinationVC.swift
+//  animations
+//
+//  Created by Vidyadhar V. Thatte on 5/21/15.
+//  Copyright (c) 2015 Vidyadhar V Thatte. All rights reserved.
+//
+
+import UIKit
+
+class TransitionDestinationVC: UIViewController {
+
+}
