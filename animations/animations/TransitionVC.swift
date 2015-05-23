8265b34f20f1f18af769f37eccf8b205737a9393
Vidyadhar Thatte <vd_thatte@yahoo.com>
2015-05-22 20:29:52 -0400

    May 22 8:29PM

--- animations/animations/TransitionVC.swift
+++ animations/animations/TransitionVC.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class TransitionVC: UIViewController {
-    
+    var theBool = true
     let transitionManager = TransitionManager()
-    
+    let trans = TransitionManager2() // up and down
     @IBAction func unwindToViewController (sender: UIStoryboardSegue){
         
     }
     
+    @IBAction func show(sender: AnyObject) {
+        
+    }
+    @IBAction func button(sender: AnyObject) {
+    }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let toViewController = segue.destinationViewController as! UIViewController
-        toViewController.transitioningDelegate = self.transitionManager
+            let toViewController = segue.destinationViewController as! UIViewController
+            toViewController.transitioningDelegate = self.transitionManager
+/*
+            let toViewController = segue.destinationViewController as! UIViewController
+            toViewController.transitioningDelegate = self.trans
+*/
     }
     
 }
