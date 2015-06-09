20e5ff1d17d7ffbd4a0dde2e9f10a69843a4fbcf
Vidyadhar Thatte <vd_thatte@yahoo.com>
2015-06-08 22:58:55 -0400

    Monday June 8 10:58PM

--- snapchat-like-swipeviews/SwiftSwipeView/ContainerViewController.swift
+++ snapchat-like-swipeviews/SwiftSwipeView/ContainerViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 class ContainerViewController: UIViewController {
     
     // Outlet used in storyboard
-    @IBOutlet var scrollView: UIScrollView?;
+    @IBOutlet var scrollView: UIScrollView?
+    @IBOutlet var scrollViewVertical: UIScrollView?
     
     override func viewDidLoad() {
         super.viewDidLoad();
@@ -18,7 +19,7 @@ class ContainerViewController: UIViewController {
         var AVc :AViewController =  AViewController(nibName: "AViewController", bundle: nil);
         var BVc :BViewController =  BViewController(nibName: "BViewController", bundle: nil);
         var CVc :CViewController =  CViewController(nibName: "CViewController", bundle: nil);
-        
+        var DVc :DViewController =  DViewController(nibName: "DViewController", bundle: nil);
         
         // 2) Add in each view to the container view hierarchy
         //    Add them in opposite order since the view hieracrhy is a stack
@@ -30,6 +31,10 @@ class ContainerViewController: UIViewController {
         self.scrollView!.addSubview(BVc.view);
         BVc.didMoveToParentViewController(self);
         
+        self.addChildViewController(DVc);
+        self.scrollViewVertical!.addSubview(DVc.view);
+        DVc.didMoveToParentViewController(self);
+        
         self.addChildViewController(AVc);
         self.scrollView!.addSubview(AVc.view);
         AVc.didMoveToParentViewController(self);
@@ -39,22 +44,29 @@ class ContainerViewController: UIViewController {
         //    with eachother inside the container view
         var adminFrame :CGRect = AVc.view.frame;
         adminFrame.origin.x = adminFrame.width;
-        BVc.view.frame = adminFrame;
+        AVc.view.frame = adminFrame;
         
         var BFrame :CGRect = BVc.view.frame;
         BFrame.origin.x = 2*BFrame.width;
         CVc.view.frame = BFrame;
         
         var CFrame : CGRect = CVc.view.frame
-        CFrame.origin.x = 0
-        CFrame.origin.y = CFrame.height
+        CFrame.origin.x = 2*CFrame.width
         CVc.view.frame = CFrame
         
+        var DFrame : CGRect = DVc.view.frame
+        DFrame.origin.y = -DFrame.height
+        DFrame.origin.x = 0
+        DVc.view.frame = DFrame
 
         // 4) Finally set the size of the scroll view that contains the frames
-        var scrollWidth: CGFloat  = 2 * self.view.frame.width
-        var scrollHeight: CGFloat  = 2 * self.view.frame.size.height
+        var scrollWidth: CGFloat  = 3 * self.view.frame.width
+        var scrollHeight: CGFloat = self.view.frame.height
         self.scrollView!.contentSize = CGSizeMake(scrollWidth, scrollHeight);
+        
+        var scrollWidthVertical: CGFloat = 2 * self.view.frame.width
+        var scrollHeightVertical: CGFloat = 2 * self.view.frame.height
+        self.scrollViewVertical!.contentSize = CGSizeMake(scrollWidthVertical, scrollHeightVertical);
     }
 
     override func didReceiveMemoryWarning() {
